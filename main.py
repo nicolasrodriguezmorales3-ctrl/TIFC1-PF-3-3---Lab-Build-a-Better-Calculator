5a8a822f76b1d37c146bb3c695b0967674cced69
nicolasrodriguezmorales3-ctrl <nicolarodriguezmorales3@gmail.com>
2026-09-11 14:50:47 -0500

    Respuesta de laboratorio 3

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 def main():
   print("Hello learners!")
 
+import math
+
+def addmultiplenumbers(numeros):
+    return sum(numeros)
+
+def multiplymultiplenumbers(numeros):
+    if not numeros:
+        return 0
+    return math.prod(numeros)
+
+def isiteven(num):
+    return num % 2 == 0 and num % 1 == 0
+
+def main():
+    print("¡Bienvenido a la calculadora!")
+
+def isitaninteger(num):
+    if isinstance(num, bool):
+        return False
+    return isinstance(num, (int, float)) and num % 1 == 0
 if __name__=="__main__":
   main()
